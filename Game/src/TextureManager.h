5e69d13146b909603ed134001f6dce40bd6f8edd
wick3dr0se <wick3dr0se@protonmail.com>
2023-11-07 19:04:35 -0800

    multiple sprite animations & render flipping

--- Game/src/TextureManager.h
+++ Game/src/TextureManager.h
@@ -5,5 +5,5 @@
 class TextureManager {
 public:
     static SDL_Texture* LoadTexture(const char* file);
-    static void Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest);
+    static void Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest, SDL_RendererFlip flip);
 };
